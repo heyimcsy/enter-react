a5f2bcd20e03583374906cfb96659c3957573bda
heyimcsy <cinnamon1995@naver.com>
2023-03-01 11:35:27 +0900

    alert 띄우자!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
+  // <---- 자바스크립트 영역 ---->
+  const clickAlert = () => {
+    alert('2회독 차입니다 !')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /* <---- HTML/JSX 영역  ---->*/
+    <div
+      style={{
+        height: '100vh',
+        display: ' flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
+      <h2>이것은 내 만든 App컴포넌트입니다</h2>
+      <button onClick={clickAlert}>클릭!</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
