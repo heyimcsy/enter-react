7edb1df7e99465805e8b3ebeb6045d2da6c861ec
heyimcsy <cinnamon1995@naver.com>
2023-03-01 17:29:40 +0900

    자식 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Child from './components/Child'
+import Son from './components/Son'
 
+function Mother() {
+  const name = 'yoonJin'
+  return <Son motherName={name} />
+}
 function App() {
   // <---- 자바스크립트 영역 ---->
   const num = 19
@@ -18,7 +23,15 @@ function App() {
       {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
       <h2>이것은 내 만든 App컴포넌트입니다</h2>
       <p>{num > 10 ? num + '은 10보다 크다' : num + '은 10보다 작다'}</p>
+      <button
+        onClick={() => {
+          alert(num > 10 ? num + '은 10보다 크다' : num + '은 10보다 작다')
+        }}
+      >
+        ???나는 무엇일까요???
+      </button>
       <Child />
+      <Mother />
     </div>
   )
 }
---
+++ src/components/Son.jsx
@@ -0,0 +1,5 @@
+function Son(happy) {
+  return <div>나는 {happy.motherName}딸 서윤이</div>
+}
+
+export default Son
