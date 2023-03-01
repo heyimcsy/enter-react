f602b89d4ea6667ad3023b4ef3136171ab780379
heyimcsy <cinnamon1995@naver.com>
2023-03-01 11:36:59 +0900

    변수화 시키기 전

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 function App() {
   // <---- 자바스크립트 영역 ---->
-  const clickAlert = () => {
-    alert('2회독 차입니다 !')
-  }
+
   return (
     /* <---- HTML/JSX 영역  ---->*/
     <div
@@ -17,7 +15,13 @@ function App() {
     >
       {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
       <h2>이것은 내 만든 App컴포넌트입니다</h2>
-      <button onClick={clickAlert}>클릭!</button>
+      <button
+        onClick={() => {
+          alert('2회독 차입니다 !')
+        }}
+      >
+        클릭!
+      </button>
     </div>
   )
 }
